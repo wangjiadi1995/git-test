280c1adea1c11dfb433c74e8229e22c4c168ab0d
wangjiadi <wangjiadi475@163.com>
2022-02-21 19:44:24 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -7,6 +7,6 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
-        System.out.println("aaaaa");
+        System.out.println("hello git2");
     }
 }
