d17ea2f7f058b603a4db8a0c5724f3d9eb657a88
wangjiadi <wangjiadi475@163.com>
2022-02-21 19:40:34 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @Author wjd
+ * @Date 2022/2/21 19:36
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("aaaaa");
+    }
+}
